f755b13f44d04e4ba10f3f50160fb34cd39b00db
namarata.sharma <namarata.sharma@192.168.1.35>
2026-02-06 16:35:37 +0530

    FirstCommit

---
+++ src/pageobjects/MyPageObject.java
@@ -0,0 +1,12 @@
+package pageobjects;
+
+import com.provar.core.testapi.annotations.*;
+
+@Page( title="My Page Object"                                
+     , summary=""
+     , relativeUrl=""
+     , connection="Admin"
+     )             
+public class MyPageObject {
+			
+}
